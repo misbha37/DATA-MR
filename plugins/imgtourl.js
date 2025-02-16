eef4fcd5eb14bd7096361c4699327972a0cd5f27
MAYANK UL HAQ <misbhaulhaq865@gmail.com>
2025-02-16 16:43:57 +0500

    Update imgtourl.js

--- plugins/imgtourl.js
+++ plugins/imgtourl.js
@@ -31,7 +31,7 @@ cmd({
     const formData = new FormData();
     formData.append("image", fs.createReadStream(tempFilePath));
 
-    const { data } = await axios.post("https://api.imgbb.com/1/upload?key=64e74dffad0f2c9698e6a3a297d4fad7", formData, {
+    const { data } = await axios.post("https://api.imgbb.com/1/upload?key=f07b8d2d9f0593bc853369f251a839de", formData, {
       headers: formData.getHeaders(),
     });
 
